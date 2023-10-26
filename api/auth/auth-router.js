4bc18780e32b957c3c1438a784060cfa51731a03
Devin Bolden <devinbolden6@gmail.com>
2023-10-25 21:17:37 -0500

    wip

--- api/auth/auth-router.js
+++ api/auth/auth-router.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken')
 
 const router = express.Router();
 
@@ -71,32 +72,66 @@ router.post('/register', async (req, res) => {
 
 
 
-
-router.post('/login', (req, res) => {
-  res.end('implement login, please!');
+router.post('/login', async (req, res) => {
   /*
-    IMPLEMENT
-    You are welcome to build additional middlewares to help with the endpoint's functionality.
-
-    1- In order to log into an existing account the client must provide `username` and `password`:
-      {
-        "username": "Captain Marvel",
-        "password": "foobar"
-      }
-
-    2- On SUCCESSFUL login,
-      the response body should have `message` and `token`:
-      {
-        "message": "welcome, Captain Marvel",
-        "token": "eyJhbGciOiJIUzI ... ETC ... vUPjZYDSa46Nwz8"
-      }
-
-    3- On FAILED login due to `username` or `password` missing from the request body,
+  IMPLEMENT
+  You are welcome to build additional middlewares to help with the endpoint's functionality.
+  
+  1- In order to log into an existing account the client must provide `username` and `password`:
+  {
+    "username": "Captain Marvel",
+    "password": "foobar"
+  }
+  
+  2- On SUCCESSFUL login,
+  the response body should have `message` and `token`:
+  {
+    "message": "welcome, Captain Marvel",
+    "token": "eyJhbGciOiJIUzI ... ETC ... vUPjZYDSa46Nwz8"
+  }
+  
+  3- On FAILED login due to `username` or `password` missing from the request body,
       the response body should include a string exactly as follows: "username and password required".
-
-    4- On FAILED login due to `username` not existing in the db, or `password` being incorrect,
+      
+      4- On FAILED login due to `username` not existing in the db, or `password` being incorrect,
       the response body should include a string exactly as follows: "invalid credentials".
-  */
+      */
+     
+     const { username, password } = req.body;
+     // Check if username and password are present
+  if (!username || !password) {
+    return res.status(400).send("username and password required");
+  }
+
+  // Find the user in the database by username
+  const user = users.find(user => user.username === username);
+
+  // Check if the user exists
+  if (!user) {
+    return res.status(401).send("invalid credentials");
+  }
+
+  try {
+    // Compare the provided password with the hashed password in the database
+    const passwordMatch = await bcrypt.compare(password, user.password);
+
+    if (passwordMatch) {
+      // Password is correct, generate a token
+      const token = jwt.sign({ username: user.username, userId: user.id }, 'your_secret_key', { expiresIn: '1h' });
+
+      // Respond with a welcome message and the token
+      res.json({
+        message: `welcome, ${user.username}`,
+        token,
+      });
+    } else {
+      // Password is incorrect
+      res.status(401).send("invalid credentials");
+    }
+  } catch (error) {
+    console.error("Error during login:", error);
+    res.status(500).send("Internal Server Error");
+  }
 });
 
 module.exports = router;
