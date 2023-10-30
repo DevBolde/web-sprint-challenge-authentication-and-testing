0097c4c0ad648b3fdd3d75d67db387878c5ba7b0
Devin Bolden <devinbolden6@gmail.com>
2023-10-30 06:02:13 -0500

    wip

--- api/auth/auth-router.js
+++ api/auth/auth-router.js
@@ -35,11 +35,9 @@ router.post('/register', async (req, res, next) => {
   */
 
    const { username, password } = req.body;
-  // Check if username and password are present
   if (!username || !password) {
     return res.status(400).json({ message: 'username and password required' });
   }
-
   try {
     const existingUser = await User.findByUsername(username);
     
@@ -49,8 +47,9 @@ router.post('/register', async (req, res, next) => {
 
     const hash = bcrypt.hashSync(password, 8);
     const newUser = { username, password: hash };
-      await User.add(newUser);
+     const result = await User.add(newUser);
     res.status(201).json({
+      id: result.id,
       message: `nice to have you, ${newUser.username}`
     });
   } catch (err) {
@@ -62,65 +61,64 @@ router.post('/register', async (req, res, next) => {
 
 
 router.post('/login', async (req, res) => {
-  /*
-  IMPLEMENT
-  You are welcome to build additional middlewares to help with the endpoint's functionality.
-  
-  1- In order to log into an existing account the client must provide `username` and `password`:
-  {
-    "username": "Captain Marvel",
-    "password": "foobar"
-  }
-  
-  2- On SUCCESSFUL login,
-  the response body should have `message` and `token`:
-  {
-    "message": "welcome, Captain Marvel",
-    "token": "eyJhbGciOiJIUzI ... ETC ... vUPjZYDSa46Nwz8"
-  }
-  
-  3- On FAILED login due to `username` or `password` missing from the request body,
-      the response body should include a string exactly as follows: "username and password required".
+      /*
+      IMPLEMENT
+      You are welcome to build additional middlewares to help with the endpoint's functionality.
       
-      4- On FAILED login due to `username` not existing in the db, or `password` being incorrect,
-      the response body should include a string exactly as follows: "invalid credentials".
-      */
-     
-     const { username, password } = req.body;
-     // Check if username and password are present
-  if (!username || !password) {
-    return res.status(400).send("username and password required");
-  }
-
-  // Find the user in the database by username
-  const user = User.find(user => user.username === username);
-
-  // Check if the user exists
-  if (!user) {
-    return res.status(401).send("invalid credentials");
-  }
-
-  try {
-    // Compare the provided password with the hashed password in the database
-    const passwordMatch = await bcrypt.compare(password, user.password);
-
-    if (passwordMatch) {
-      // Password is correct, generate a token
-      const token = jwt.sign({ username: user.username, userId: user.id }, 'your_secret_key', { expiresIn: '1h' });
-
-      // Respond with a welcome message and the token
-      res.json({
-        message: `welcome, ${user.username}`,
-        token,
-      });
-    } else {
-      // Password is incorrect
-      res.status(401).send("invalid credentials");
-    }
-  } catch (error) {
-    console.error("Error during login:", error);
-    res.status(500).send("Internal Server Error");
-  }
-});
-
+      1- In order to log into an existing account the client must provide `username` and `password`:
+      {
+        "username": "Captain Marvel",
+        "password": "foobar"
+      }
+      
+      2- On SUCCESSFUL login,
+      the response body should have `message` and `token`:
+      {
+        "message": "welcome, Captain Marvel",
+        "token": "eyJhbGciOiJIUzI ... ETC ... vUPjZYDSa46Nwz8"
+      }
+      
+      3- On FAILED login due to `username` or `password` missing from the request body,
+          the response body should include a string exactly as follows: "username and password required".
+          
+          4- On FAILED login due to `username` not existing in the db, or `password` being incorrect,
+          the response body should include a string exactly as follows: "invalid credentials".
+          */
+      const { username, password } = req.body;
+    
+      // Check if username and password are present
+      if (!username || !password) {
+        return res.status(400).send("username and password required");
+      }
+    
+      try {
+        // Find the user in the database by username
+        const user = await User.findByUsername(username);
+    
+        // Check if the user exists
+        if (!user) {
+          return res.status(401).send("invalid credentials");
+        }
+    
+        // Compare the provided password with the hashed password in the database
+        const passwordMatch = await bcrypt.compare(password, user.password);
+    
+        if (passwordMatch) {
+          // Password is correct, generate a token
+          const token = jwt.sign({ username: user.username, userId: user.id }, 'your_secret_key', { expiresIn: '1h' });
+    
+          // Respond with a welcome message and the token
+          res.json({
+            message: `welcome, ${user.username}`,
+            token,
+          });
+        } else {
+          // Password is incorrect
+          res.status(401).send("invalid credentials");
+        }
+      } catch (error) {
+        console.error("Error during login:", error);
+        res.status(500).send("Internal Server Error");
+      }
+    });
 module.exports = router;
