1754ad87816a4d99b38e18db10962ca6ce39b691
Devin Bolden <devinbolden6@gmail.com>
2023-10-30 21:23:45 -0500

    wip

--- api/server.test.js
+++ api/server.test.js
@@ -1,8 +1,5 @@
 // Write your tests here
-test('sanity', () => {
-  expect(true).toBe(false)
-})
-
-// describe('test for register endpoint', () => {
-//   test('username and password can ')
+// test('sanity', () => {
+//   expect(true).toBe(false)
 // })
+
