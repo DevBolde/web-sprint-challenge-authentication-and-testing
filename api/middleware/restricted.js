13df0e148dfd49b3b7178ad4ba25a4b34e66127c
Devin Bolden <devinbolden6@gmail.com>
2023-11-02 01:28:22 -0500

    wip

--- api/middleware/restricted.js
+++ api/middleware/restricted.js
@@ -2,37 +2,20 @@ const jwt = require('jsonwebtoken')
 const { JWT_SECRET } = require('../secrets/secret')
 
 module.exports = (req, res, next) => {
-  /*
-    IMPLEMENT
+  const authHeader = req.headers.authorization;
 
-    1- On valid token in the Authorization header, call next.
-      
-    2- On missing token in the Authorization header,
-      the response body should include a string exactly as follows: "token required".
+  if (!authHeader || !authHeader.startsWith('Bearer ')) {
+    return res.status(401).json({ message: 'token required' });
+  }
 
-    3- On invalid or expired token in the Authorization header,
-      the response body should include a string exactly as follows: "token invalid".
-  */
- const authHeader = req.headers.authorization;
+  const token = authHeader.split(' ')[1];
 
- if(!authHeader){
-  return res.json({ status: 401, message: 'token required'})
- }
- const token = authHeader.split(' ')[1];
+  jwt.verify(token, JWT_SECRET, (err, decoded) => {
+    if (err) {
+      return res.status(401).json({ message: 'token invalid' });
+    }
 
- if(!isValidToken(token)){
-  return res.json({ status: 401, message: 'token invalid'})
- }
- 
- req.token = token; 
- next()
-
- function isValidToken(token) {
-  try{
-    jwt.verify(token, JWT_SECRET)
-    return true;
-  } catch(err){
-  return false;
- }
- } 
+    req.token = decoded;
+    next();
+  });
 };
--- api/server.test.js
+++ api/server.test.js
@@ -31,4 +31,37 @@ describe('register endpoint', () => {
 });
 
 
+describe('login endpoint', () => {
+  beforeAll(async () => {
+    await db.migrate.latest() // If using knex, migrate to latest db state
+  });
 
+  afterAll(async () => {
+    await db.migrate.rollback() // Rollback to initial db state after tests are done
+  });
+
+  it('should log in with correct credentials', async () => {
+    // Register a new user
+    await request(server)
+      .post('/api/auth/register') // Replace with your register endpoint path
+      .send({ username: 'testuser', password: 'testpass' }); // Replace with your user schema
+
+    // Attempt to log in with the new user
+    const res = await request(server)
+      .post('/api/auth/login') // Replace with your login endpoint path
+      .send({ username: 'testuser', password: 'testpass' }); // Replace with your user schema
+
+    expect(res.statusCode).toEqual(200);
+    expect(res.body.message).toEqual('welcome, testuser');
+    expect(res.body).toHaveProperty('token');
+  });
+
+  it('should not log in with incorrect password', async () => {
+    const res = await request(server)
+      .post('/api/auth/login') // Replace with your login endpoint path
+      .send({ username: 'testuser', password: 'wrongpass' }); // Replace with your user schema
+
+    expect(res.statusCode).toEqual(401);
+    expect(res.body).toEqual({ message: 'invalid credentials' }); // Replace with your error message
+  });
+});
