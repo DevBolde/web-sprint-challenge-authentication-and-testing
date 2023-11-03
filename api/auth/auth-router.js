238aca5c10fd119256319649235dfa27cc38e6b7
Devin Bolden <devinbolden6@gmail.com>
2023-11-03 10:01:18 -0500

    finished

--- api/auth/auth-router.js
+++ api/auth/auth-router.js
@@ -119,15 +119,5 @@ router.post('/login', async (req, res) => {
       }
     });
 
-    // function buildToken(user) {
-    //   const payload  = {
-    //     subject: user.id, 
-    //     username: user.username,
-    //     role: user.role
-    //   }
-    //   const options = {
-    //     expiresIn: '1d',
-    //   }
-    //   return jwt.sign(payload, JWT_SECRET ,options)
-    // }
+    
 module.exports = router;
